2db14991e041418edbf4628253123a53abeb5c32
moazSalah <moazsns92@gmail.com>
2020-11-11 16:20:08 -0800

    add job views and url

--- job/admin.py
+++ job/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Job 
+from .models import Job , Ctegory
 
 admin.site.register(Job)
+
+admin.site.register(Ctegory)
--- job/models.py
+++ job/models.py
@@ -4,6 +4,8 @@ from django.db import models
 JOB_TYPE = (
 	('Full_time','Full_time'),
 	('Part_time','Part_time'))
+
+
 class Job(models.Model):
  	title = models.CharField(max_length = 100)
  	#location 
@@ -13,7 +15,15 @@ class Job(models.Model):
  	vacancy = models.IntegerField(default=1)
  	salary = models.IntegerField(default = 0)
  	experiance = models.IntegerField(default = 1)
+ 	category = models.ForeignKey('Ctegory', on_delete = models.CASCADE)
 
 
  	def __str__(self):
  		return self.title
+
+
+class Ctegory(models.Model):
+	name = models.CharField(max_length = 40)
+
+	def __str__(self):
+		return self.name
